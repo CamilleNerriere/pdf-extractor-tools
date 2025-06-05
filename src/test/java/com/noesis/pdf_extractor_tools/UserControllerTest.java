36de9942d25995933dbbf97d6e2efc87b118e957
CamilleNerriere <camille.nerriere@yahoo.fr>
2025-06-05 16:09:15 +0200

    add user test

---
+++ src/test/java/com/noesis/pdf_extractor_tools/UserControllerTest.java
@@ -0,0 +1,57 @@
+package com.noesis.pdf_extractor_tools;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.noesis.pdf_extractor_tools.service.JwtService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtService jwtService;
+
+    private String token;
+
+    @BeforeEach
+    void setup() {
+        UserDetails userDetails = User.builder().username("demo.user@example.com").password("superSecretPassword")
+                .roles("USER").build();
+        token = jwtService.generateToken(userDetails);
+    }
+
+    @Test
+    void shouldReturnUser() throws Exception {
+        mockMvc.perform(get("/user/me").header("Authorization", "Bearer " + token)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstname").value("Michel"));
+    }
+
+    @Test
+    void shouldUpdateUser() throws Exception {
+        String json = "{\"firstname\":\"NewName\"}";
+        mockMvc.perform(put("/user/update")
+                .header("Authorization", "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstname").value("NewName"));
+    }
+}
