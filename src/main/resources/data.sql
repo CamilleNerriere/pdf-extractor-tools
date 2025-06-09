93378a4e020790884fadc5f304c82e41c71b05e1
CamilleNerriere <camille.nerriere@yahoo.fr>
2025-06-09 15:54:46 +0200

    include data for demo

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (firstname, lastname, username, email, password) VALUES 
+('Michel', 'Foucault', 'michelF', 'demo.user@example.com', '$argon2id$v=19$m=65536,t=2,p=1$hv5TFI10yda2kCTl1lNIXw$yQ9cljW9DN4YfeMC6ySTZTWT585TGVbUonoQARuFYVE');
+
