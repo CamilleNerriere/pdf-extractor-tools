575ac49aac49dbfe1228c7d8158cd98724a946e5
CamilleNerriere <camille.nerriere@yahoo.fr>
2025-06-11 20:40:12 +0200

    fix cors policy

--- src/main/java/com/noesis/pdf_extractor_tools/config/SecurityConfig.java
+++ src/main/java/com/noesis/pdf_extractor_tools/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.noesis.pdf_extractor_tools.config;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,36 +14,37 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import com.noesis.pdf_extractor_tools.security.JwtAuthentificationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-    @Value("${allowed.origins}")
-    private String allowedOrigins;
+    // @Value("${allowed.origins}")
+    // private String allowedOrigins;
 
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOriginPatterns(List.of(
                 "https://noesis-pdf-tools.vercel.app",
                 "http://localhost:4200"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
 
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        config.setAllowCredentials(false); // no cookies right now/turn to true then
+        config.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        return source;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthentificationFilter jwtAuthentificationFilter)
+    public SecurityFilterChain filterChain(HttpSecurity http,
+            JwtAuthentificationFilter jwtAuthentificationFilter)
             throws Exception {
         http
                 .cors(Customizer.withDefaults())
@@ -54,7 +54,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/**", "/error").permitAll()
                         .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthentificationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthentificationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
